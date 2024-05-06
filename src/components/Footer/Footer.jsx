1b6828ceec4f4dde0aa5db9442223f3b1d5450e8
AysKrimn <akabilmemne@gmail.com>
2024-05-06 13:25:37 +0300

    footer güncellendi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
-    return <div className='footer'>© 2024 wlanHan</div>;
+    return <div className='footer'>© 2024 wlanHan & Omer</div>;
 
 }
 
